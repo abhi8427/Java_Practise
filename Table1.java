bc15dce4d0f8624e23c6a2b7632d22fafc71aad5
Abhishek <abhishek.coolabhi@gmail.com>
2022-12-11 00:21:06 +0530

    to print a table

---
+++ Table1.java
@@ -0,0 +1,12 @@
+public class Table1 {
+
+    public static void main (String abhi[]){
+
+        for(int i = 1; i<=10; i++){
+
+            int j = 2*i;
+            System.out.println(j);
+        }
+    }
+    
+}
