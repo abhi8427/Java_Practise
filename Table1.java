235bce4edc22d92cebac3098854b18bb22297c64
abhi8427 <abhishek.coolabhi@gmail.com>
2022-12-10 23:51:26 +0530

    Add files via upload

---
+++ Table1.java
@@ -0,0 +1,18 @@
+
+public class Table1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<=10; i++){
+			
+			int j = i*2;
+			System.out.println(j);
+		}
+
+	}
+
+}
