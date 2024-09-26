742d2aa9a512e9d45efd89b5884edba6f77ed785
Abhishek <abhishek.coolabhi@gmail.com>
2024-09-26 11:48:50 +0530

    Printed cities

--- arrayList003.java
+++ arrayList003.java
@@ -18,28 +18,28 @@ public class arrayList003 {
 		System.out.println("The total cities are "+size);
 		System.out.println("The number of cities I have lived "+city);
 		
-		if(city.isEmpty()){
+		/*if(city.isEmpty()){
 			System.out.println("There is no city");
 		}else{
 			System.out.println("There are cities avilable");
-		}
+		}*/
 		
-		if(city.contains("Brampton")){
+		/*if(city.contains("Brampton")){
 			System.out.println("You also lived in CaNADa");
 		}else{
 			System.out.println("You have never travel to abroad");
-		}
+		}*/
 		
-		city.add(1, "Lucknow");
-		city.add(2, "Allahabad");
+		//city.add(1, "Lucknow");
+		//city.add(2, "Allahabad");
 		
-		System.out.println("The new list of the cities "+city);
+		//System.out.println("The new list of the cities "+city);
 		
-		city.set(5, "Saskatchewan");
-		System.out.println("The new list of cities " +city);
+		//city.set(5, "Saskatchewan");
+		//System.out.println("The new list of cities " +city);
 		
-		city.remove("Delhi");
-		System.out.println("The updated city are "+city);
+		//city.remove("Delhi");
+		//System.out.println("The updated city are "+city);
 	}
 
 }
