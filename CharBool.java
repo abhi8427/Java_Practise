1b131fbac993be7e3e50d91bfa4bb323df263f07
Abhishek <abhishek.coolabhi@gmail.com>
2024-11-02 20:56:48 +0530

    Character and Boolean variables

---
+++ CharBool.java
@@ -0,0 +1,20 @@
+package Nov2nd;
+
+public class CharBool {
+	
+	public static void main (String args[]){
+		
+		char c = '5';
+		
+		System.out.println(c);
+		
+		boolean b = false;
+		
+		if (b){
+			System.out.println("Abhishek");
+		}else{
+			System.out.println("Sharma");
+		}
+	}
+
+}
