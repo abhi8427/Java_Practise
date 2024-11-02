2ee948d677b3ada3b734328ad894457aed1539bf
Abhishek <abhishek.coolabhi@gmail.com>
2024-11-02 20:06:43 +0530

    Added Primitive Data Type program

---
+++ PrimData.java
@@ -0,0 +1,16 @@
+package Nov2nd;
+
+public class PrimData {
+	
+	public static void main (String args[]){
+		byte a = 100;
+		System.out.println(a);
+		short b = 2000;
+		System.out.println(b);
+		int i = 5000;
+		System.out.println(i);
+		long j = 8L;
+		System.out.println(j);
+	}
+
+}
