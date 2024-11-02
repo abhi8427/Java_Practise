14636b856cfa04763d311b0587f5444045e3fef6
Abhishek <abhishek.coolabhi@gmail.com>
2024-11-02 17:18:29 +0530

    Square Root Program

---
+++ SquareRoot.java
@@ -0,0 +1,16 @@
+package Nov2nd;
+
+public class SquareRoot {
+	
+	public static void main(String args[]){
+		double x = 5;
+		double y;
+		double z;
+		
+		y = Math.sqrt(x);
+		System.out.println(y);
+		z = Math.random();
+		System.out.println(z);
+	}
+
+}
