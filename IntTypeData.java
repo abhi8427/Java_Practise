0375d518058e8aa9732ab4c9ebab6e482fee13eb
Abhishek <abhishek.coolabhi@gmail.com>
2024-11-02 20:30:23 +0530

    integer data type variables

---
+++ IntTypeData.java
@@ -0,0 +1,23 @@
+package Nov2nd;
+
+public class IntTypeData {
+	
+	public static void main(String ab[]){
+		
+		byte b = 2;
+		System.out.println(b);
+		short s = 4;
+		System.out.println(s);
+		int i = 6;
+		System.out.println(i);
+		long l = 8l;
+		System.out.println(l);
+		float f = 10;
+		System.out.println(f);
+		double d = 12;
+		System.out.println(d);
+		
+		
+	}
+
+}
