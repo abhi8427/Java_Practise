752844a2ee43cf50f8a21e74325fdef9f0499d38
Abhishek <abhishek.coolabhi@gmail.com>
2024-11-02 18:32:22 +0530

    Constants and exponentials

---
+++ Exp.java
@@ -0,0 +1,16 @@
+package Nov2nd;
+
+public class Exp {
+	
+	public static void main(String args[]){
+		double x = 7E2;
+		System.out.println(x);
+		System.out.println('\b');
+		System.out.println('\f');
+		System.out.println('\n');
+		System.out.println('\r');
+		System.out.println('\t');
+		
+	}
+
+}
